3f4a0a4c7883eaa0cea0868f6e8bddbcace3d266
yahui <tianya0yingzi@gmail.com>
2018-12-19 21:10:31 +0800

    first version minstack

---
+++ min_stack/get_min.py
@@ -0,0 +1,53 @@
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.ary = []
+        self.min = None
+        self.need_update = False
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.ary.append(x)
+        if not self.need_update:
+            if self.min is None or self.min > x:
+                 self.min = x
+       
+    def pop(self):
+        """
+        :rtype: void
+        """
+        r = self.ary.pop()
+        
+        if not self.need_update and r == self.min:
+            self.min = None
+            self.need_update = True
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if len(self.ary) > 0:
+            return self.ary[-1]
+        else:
+            return None
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if self.need_update:
+            return self.updateAndFindMin()
+        else:
+            return self.min
+
+    def updateAndFindMin(self):
+        ary = sorted(self.ary)
+        self.min = ary[0]
+        self.need_update = False
+        return self.min
